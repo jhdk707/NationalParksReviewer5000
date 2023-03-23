28f236413fed3139e4db83476ff5eb60318b3664
pmgirardi <117805446+pmgirardi@users.noreply.github.com>
2023-03-22 20:47:48 -0700

    added comment, post, user, and index models

---
+++ Develop/models/index.js
@@ -0,0 +1,34 @@
+const path = require('path')
+let currentPath = path.join(__dirname)
+const Comment = require('./Comment');
+const Post = require('./Post');
+const User = require('./User');
+
+console.log(require('./Comment'));
+
+//post - user
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+//user - comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+//post belongs to user
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+//has many comments
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+//comment belongs to post
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+//comment belongs to user
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { Post, Comment, User};
---
+++ Develop/models/user.js
@@ -0,0 +1,60 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcryptjs');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+//checks for password
+  checkPassword(loginPasswd) {
+    return bcrypt.compareSync(loginPasswd, this.password);
+  }
+}
+
+//User model
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4],
+      },
+    },
+  },
+  {
+    hooks: {
+        async beforeCreate(newUser){
+        newUser.password = await bcrypt.hash(newUser.password, 10);
+        return newUser;
+      },
+       async beforeUpdate (updatedUserData){
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
