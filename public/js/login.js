e868a0db8131f90dabb8798b20542694e73f1c79
Doomtower <jessehudak707@outlook.com>
2023-04-02 16:30:31 -0700

    added header style, logo, navbar, fixed background size. still need to style homepage list and post p[ages, connect contact page

--- public/js/login.js
+++ public/js/login.js
@@ -1,24 +1,33 @@
-async function loginFormHandler(event) {
-  event.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+  async function loginFormHandler(event) {
+    event.preventDefault();
 
-  const email = document.querySelector("#email-login").value.trim();
-  const password = document.querySelector("#password-login").value.trim();
-  console.log(email, password);
-  if (email && password) {
-    const response = await fetch("/api/user/login", {
-      method: "POST",
-      body: JSON.stringify({ email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
+    const emailInput = document.querySelector("#email-login");
+    const passwordInput = document.querySelector("#password-login");
 
-    if (response.ok) {
-      document.location.replace("/dashboard");
-    } else {
-      alert(response.statusText);
+    if (emailInput && passwordInput) {
+      const email = emailInput.value.trim();
+      const password = passwordInput.value.trim();
+      console.log(email, password);
+
+      if (email && password) {
+        const response = await fetch("/api/user/login", {
+          method: "POST",
+          body: JSON.stringify({ email, password }),
+          headers: { "Content-Type": "application/json" },
+        });
+
+        if (response.ok) {
+          document.location.replace("/dashboard");
+        } else {
+          alert(response.statusText);
+        }
+      }
     }
   }
-}
 
-document
-  .querySelector(".login-form")
-  .addEventListener("submit", loginFormHandler);
+  const loginForm = document.querySelector(".login-form");
+  if (loginForm) {
+    loginForm.addEventListener("submit", loginFormHandler);
+  }
+});
