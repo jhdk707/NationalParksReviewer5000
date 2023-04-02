0fa138f826437f1e8fec21f4fb3377f323c62d6b
Doomtower <jessehudak707@outlook.com>
2023-04-01 23:16:38 -0700

    implementing BULMA css, login and signup forms fixed. background still needs work, comments pages need work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  //content: ["./dist/**/*.{html,js}"],
-  content: ["./views/**/*.{html}", "./public/**/*.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   //content: ["./dist/**/*.{html,js}"],
+//   content: ["./views/**/*.{html}", "./public/**/*.js"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
